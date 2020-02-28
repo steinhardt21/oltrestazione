0404f71c1ab453be1d2f978884fca73e2af90550
Alexandru Enache <44054368+steinhardt21@users.noreply.github.com>
2020-02-28 16:15:14 +0100

    Version 0.7

---
+++ script.js
@@ -0,0 +1,19 @@
+// console.log("Hello");
+
+$("#dot_1").hover(function()
+    {
+      $("#mt_2, #mt_3,#mt_4, #mt_5, #mt_6, #mt_7, #mt_8, #mt_9, #mt_10, #mt_11, #mt_12, #mt_13, #mt_14, #mt_15, #mt_16, #mt_17, #mt_18, #mt_19, #mt_20, #mt_21, #mt_22, #mt_23, #mt_24, #dot_2, #dot_3, #dot_4, #dot_5, #dot_6 ").css("opacity", "0.2");
+    }, function()
+    {
+      $("#mt_2, #mt_3,#mt_4, #mt_5, #mt_6, #mt_7, #mt_8, #mt_9, #mt_10, #mt_11, #mt_12, #mt_13, #mt_14, #mt_15, #mt_16, #mt_17, #mt_18, #mt_19, #mt_20, #mt_21, #mt_22, #mt_23, #mt_24, #dot_2, #dot_3, #dot_4, #dot_5, #dot_6 ").css("opacity", "1.0");
+    }
+  )
+
+  $("#dot_2").hover(function()
+      {
+        $("#mt_1, #mt_3,#mt_4, #mt_5, #mt_6, #mt_7, #mt_8, #mt_9, #mt_10, #mt_11, #mt_12, #mt_13, #mt_14, #mt_15, #mt_16, #mt_17, #mt_18, #mt_19, #mt_20, #mt_21, #mt_22, #mt_23, #mt_24, #dot_1, #dot_3, #dot_4, #dot_5, #dot_6 ").css("opacity", "0.2");
+      }, function()
+      {
+        $("#mt_1, #mt_3,#mt_4, #mt_5, #mt_6, #mt_7, #mt_8, #mt_9, #mt_10, #mt_11, #mt_12, #mt_13, #mt_14, #mt_15, #mt_16, #mt_17, #mt_18, #mt_19, #mt_20, #mt_21, #mt_22, #mt_23, #mt_24, #dot_1, #dot_3, #dot_4, #dot_5, #dot_6 ").css("opacity", "1.0");
+      }
+    )
