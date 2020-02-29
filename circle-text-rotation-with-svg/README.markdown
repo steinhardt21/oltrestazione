f7c9b1abaf238a8b5bccd24a60747ed94c605972
Alexandru Enache <44054368+steinhardt21@users.noreply.github.com>
2020-03-01 00:30:25 +0100

    Add files via upload

---
+++ circle-text-rotation-with-svg/README.markdown
@@ -0,0 +1,4 @@
+# Circle text rotation with SVG 
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/Shaikat/pen/PapGpr](https://codepen.io/Shaikat/pen/PapGpr).
+
+ 
--- script.js
+++ script.js
@@ -2,18 +2,156 @@
 
 $("#dot_1").hover(function()
     {
-      $("#mt_2, #mt_3,#mt_4, #mt_5, #mt_6, #mt_7, #mt_8, #mt_9, #mt_10, #mt_11, #mt_12, #mt_13, #mt_14, #mt_15, #mt_16, #mt_17, #mt_18, #mt_19, #mt_20, #mt_21, #mt_22, #mt_23, #mt_24, #dot_2, #dot_3, #dot_4, #dot_5, #dot_6 ").css("opacity", "0.2");
+      $("#mt_2, #mt_3,#mt_4, #mt_5, #mt_6, #mt_7, #mt_8, #mt_9, #mt_10, #mt_11, #mt_12, #mt_13, #mt_14, #mt_15, #mt_16, #mt_17, #dot_2, #dot_3, #dot_4, #dot_5, #dot_6, #dot_7, #dot_8, #dot_9, #dot_10, #dot_11, #dot_12, #dot_13, #dot_14, #dot_15, #dot_16, #dot_17, #dot_18, #dot_19, #dot_20, #dot_21, #dot_22, #dot_23, #dot_24").css("opacity", "0.2");
     }, function()
     {
-      $("#mt_2, #mt_3,#mt_4, #mt_5, #mt_6, #mt_7, #mt_8, #mt_9, #mt_10, #mt_11, #mt_12, #mt_13, #mt_14, #mt_15, #mt_16, #mt_17, #mt_18, #mt_19, #mt_20, #mt_21, #mt_22, #mt_23, #mt_24, #dot_2, #dot_3, #dot_4, #dot_5, #dot_6 ").css("opacity", "1.0");
+      $("#mt_2, #mt_3,#mt_4, #mt_5, #mt_6, #mt_7, #mt_8, #mt_9, #mt_10, #mt_11, #mt_12, #mt_13, #mt_14, #mt_15, #mt_16, #mt_17, #dot_2, #dot_3, #dot_4, #dot_5, #dot_6, #dot_7, #dot_8, #dot_9, #dot_10, #dot_11, #dot_12, #dot_13, #dot_14, #dot_15, #dot_16, #dot_17").css("opacity", "1.0");
     }
   )
 
   $("#dot_2").hover(function()
       {
-        $("#mt_1, #mt_3,#mt_4, #mt_5, #mt_6, #mt_7, #mt_8, #mt_9, #mt_10, #mt_11, #mt_12, #mt_13, #mt_14, #mt_15, #mt_16, #mt_17, #mt_18, #mt_19, #mt_20, #mt_21, #mt_22, #mt_23, #mt_24, #dot_1, #dot_3, #dot_4, #dot_5, #dot_6 ").css("opacity", "0.2");
+        $("#mt_1, #mt_3,#mt_4, #mt_5, #mt_6, #mt_7, #mt_8, #mt_9, #mt_10, #mt_11, #mt_12, #mt_13, #mt_14, #mt_15, #mt_16, #mt_17, #dot_1, #dot_3, #dot_4, #dot_5, #dot_6, #dot_7, #dot_8, #dot_9, #dot_10, #dot_11, #dot_12, #dot_13, #dot_14, #dot_15, #dot_16, #dot_17, #dot_18, #dot_19, #dot_20, #dot_21, #dot_22, #dot_23, #dot_24").css("opacity", "0.2");
       }, function()
       {
-        $("#mt_1, #mt_3,#mt_4, #mt_5, #mt_6, #mt_7, #mt_8, #mt_9, #mt_10, #mt_11, #mt_12, #mt_13, #mt_14, #mt_15, #mt_16, #mt_17, #mt_18, #mt_19, #mt_20, #mt_21, #mt_22, #mt_23, #mt_24, #dot_1, #dot_3, #dot_4, #dot_5, #dot_6 ").css("opacity", "1.0");
+        $("#mt_1, #mt_3,#mt_4, #mt_5, #mt_6, #mt_7, #mt_8, #mt_9, #mt_10, #mt_11, #mt_12, #mt_13, #mt_14, #mt_15, #mt_16, #mt_17, #dot_1, #dot_3, #dot_4, #dot_5, #dot_6, #dot_7, #dot_8, #dot_9, #dot_10, #dot_11, #dot_12, #dot_13, #dot_14, #dot_15, #dot_16, #dot_17, #dot_18, #dot_19, #dot_20, #dot_21, #dot_22, #dot_23, #dot_24").css("opacity", "1.0");
       }
     )
+
+    $("#dot_3").hover(function()
+        {
+          $("#mt_1, #mt_2,#mt_4, #mt_5, #mt_6, #mt_7, #mt_8, #mt_9, #mt_10, #mt_11, #mt_12, #mt_13, #mt_14, #mt_15, #mt_16, #mt_17, #dot_1, #dot_2, #dot_4, #dot_5, #dot_6, #dot_7, #dot_8, #dot_9, #dot_10, #dot_11, #dot_12, #dot_13, #dot_14, #dot_15, #dot_16, #dot_17, #dot_18, #dot_19, #dot_20, #dot_21, #dot_22, #dot_23, #dot_24").css("opacity", "0.2");
+        }, function()
+        {
+          $("#mt_1, #mt_2,#mt_4, #mt_5, #mt_6, #mt_7, #mt_8, #mt_9, #mt_10, #mt_11, #mt_12, #mt_13, #mt_14, #mt_15, #mt_16, #mt_17, #dot_1, #dot_2, #dot_4, #dot_5, #dot_6, #dot_7, #dot_8, #dot_9, #dot_10, #dot_11, #dot_12, #dot_13, #dot_14, #dot_15, #dot_16, #dot_17, #dot_18, #dot_19, #dot_20, #dot_21, #dot_22, #dot_23, #dot_24").css("opacity", "1.0");
+        }
+      )
+
+
+$("#dot_4").hover(function()
+    {
+      $("#mt_1, #mt_3, #mt_2, #mt_5, #mt_6, #mt_7, #mt_8, #mt_9, #mt_10, #mt_11, #mt_12, #mt_13, #mt_14, #mt_15, #mt_16, #mt_17, #dot_1, #dot_3, #dot_2, #dot_5, #dot_6, #dot_7, #dot_8, #dot_9, #dot_10, #dot_11, #dot_12, #dot_13, #dot_14, #dot_15, #dot_16, #dot_17, #dot_18, #dot_19, #dot_20, #dot_21, #dot_22, #dot_23, #dot_24").css("opacity", "0.2");
+    }, function()
+    {
+      $("#mt_1, #mt_3,#mt_2, #mt_5, #mt_6, #mt_7, #mt_8, #mt_9, #mt_10, #mt_11, #mt_12, #mt_13, #mt_14, #mt_15, #mt_16, #mt_17, #dot_1, #dot_3, #dot_2, #dot_5, #dot_6, #dot_7, #dot_8, #dot_9, #dot_10, #dot_11, #dot_12, #dot_13, #dot_14, #dot_15, #dot_16, #dot_17, #dot_18, #dot_19, #dot_20, #dot_21, #dot_22, #dot_23, #dot_24").css("opacity", "1.0");
+    }
+  )
+
+
+$("#dot_5").hover(function()
+  {
+    $("#mt_1, #mt_3,#mt_4, #mt_2, #mt_6, #mt_7, #mt_8, #mt_9, #mt_10, #mt_11, #mt_12, #mt_13, #mt_14, #mt_15, #mt_16, #mt_17,#dot_1, #dot_3, #dot_4, #dot_2, #dot_6, #dot_7, #dot_8, #dot_9, #dot_10, #dot_11, #dot_12, #dot_13, #dot_14, #dot_15, #dot_16, #dot_17, #dot_18, #dot_19, #dot_20, #dot_21, #dot_22, #dot_23, #dot_24").css("opacity", "0.2");
+  }, function()
+  {
+    $("#mt_1, #mt_3,#mt_4, #mt_2, #mt_6, #mt_7, #mt_8, #mt_9, #mt_10, #mt_11, #mt_12, #mt_13, #mt_14, #mt_15, #mt_16, #mt_17, #dot_1, #dot_3, #dot_4, #dot_2, #dot_6, #dot_7, #dot_8, #dot_9, #dot_10, #dot_11, #dot_12, #dot_13, #dot_14, #dot_15, #dot_16, #dot_17").css("opacity", "1.0");
+  }
+)
+
+
+$("#dot_6").hover(function()
+    {
+      $("#mt_1, #mt_3,#mt_4, #mt_5, #mt_2, #mt_7, #mt_8, #mt_9, #mt_10, #mt_11, #mt_12, #mt_13, #mt_14, #mt_15, #mt_16, #mt_17,#dot_1, #dot_3, #dot_4, #dot_5, #dot_2, #dot_7, #dot_8, #dot_9, #dot_10, #dot_11, #dot_12, #dot_13, #dot_14, #dot_15, #dot_16, #dot_17, #dot_18, #dot_19, #dot_20, #dot_21, #dot_22, #dot_23, #dot_24").css("opacity", "0.2");
+    }, function()
+    {
+      $("#mt_1, #mt_3,#mt_4, #mt_5, #mt_2, #mt_7, #mt_8, #mt_9, #mt_10, #mt_11, #mt_12, #mt_13, #mt_14, #mt_15, #mt_16, #mt_17, #dot_1, #dot_3, #dot_4, #dot_5, #dot_2, #dot_7, #dot_8, #dot_9, #dot_10, #dot_11, #dot_12, #dot_13, #dot_14, #dot_15, #dot_16, #dot_17").css("opacity", "1.0");
+    }
+  )
+
+  $("#dot_7").hover(function()
+      {
+        $("#mt_1, #mt_3,#mt_4, #mt_5, #mt_2, #mt_6, #mt_8, #mt_9, #mt_10, #mt_11, #mt_12, #mt_13, #mt_14, #mt_15, #mt_16, #mt_17, #dot_1, #dot_3, #dot_4, #dot_5, #dot_2, #dot_6, #dot_8, #dot_9, #dot_10, #dot_11, #dot_12, #dot_13, #dot_14, #dot_15, #dot_16, #dot_17, #dot_18, #dot_19, #dot_20, #dot_21, #dot_22, #dot_23, #dot_24").css("opacity", "0.2");
+      }, function()
+      {
+        $("#mt_1, #mt_3,#mt_4, #mt_5, #mt_2, #mt_6, #mt_8, #mt_9, #mt_10, #mt_11, #mt_12, #mt_13, #mt_14, #mt_15, #mt_16, #mt_17, #dot_1, #dot_3, #dot_4, #dot_5, #dot_2, #dot_6, #dot_8, #dot_9, #dot_10, #dot_11, #dot_12, #dot_13, #dot_14, #dot_15, #dot_16, #dot_17").css("opacity", "1.0");
+      }
+    )
+
+$("#dot_8").hover(function()
+    {
+      $("#mt_1, #mt_3,#mt_4, #mt_5, #mt_2, #mt_7, #mt_6, #mt_9, #mt_10, #mt_11, #mt_12, #mt_13, #mt_14, #mt_15, #mt_16, #mt_17, #dot_1, #dot_3, #dot_4, #dot_5, #dot_2, #dot_7, #dot_6, #dot_9, #dot_10, #dot_11, #dot_12, #dot_13, #dot_14, #dot_15, #dot_16, #dot_17, #dot_18, #dot_19, #dot_20, #dot_21, #dot_22, #dot_23, #dot_24").css("opacity", "0.2");
+    }, function()
+    {
+      $("#mt_1, #mt_3,#mt_4, #mt_5, #mt_2, #mt_7, #mt_6, #mt_9, #mt_10, #mt_11, #mt_12, #mt_13, #mt_14, #mt_15, #mt_16, #mt_17,#dot_1, #dot_3, #dot_4, #dot_5, #dot_2, #dot_7, #dot_6, #dot_9, #dot_10, #dot_11, #dot_12, #dot_13, #dot_14, #dot_15, #dot_16, #dot_17").css("opacity", "1.0");
+    }
+  )
+
+  $("#dot_9").hover(function()
+      {
+        $("#mt_1, #mt_3,#mt_4, #mt_5, #mt_2, #mt_7, #mt_8, #mt_6, #mt_10, #mt_11, #mt_12, #mt_13, #mt_14, #mt_15, #mt_16, #mt_17, #dot_1, #dot_3, #dot_4, #dot_5, #dot_2, #dot_7, #dot_8, #dot_6, #dot_10, #dot_11, #dot_12, #dot_13, #dot_14, #dot_15, #dot_16, #dot_17, #dot_18, #dot_19, #dot_20, #dot_21, #dot_22, #dot_23, #dot_24").css("opacity", "0.2");
+      }, function()
+      {
+        $("#mt_1, #mt_3,#mt_4, #mt_5, #mt_2, #mt_7, #mt_8, #mt_6, #mt_10, #mt_11, #mt_12, #mt_13, #mt_14, #mt_15, #mt_16, #mt_17,#dot_1, #dot_3, #dot_4, #dot_5, #dot_2, #dot_7, #dot_8, #dot_6, #dot_10, #dot_11, #dot_12, #dot_13, #dot_14, #dot_15, #dot_16, #dot_17").css("opacity", "1.0");
+      }
+    )
+
+    $("#dot_10").hover(function()
+        {
+          $("#mt_1, #mt_3,#mt_4, #mt_5, #mt_2, #mt_7, #mt_8, #mt_9, #mt_6, #mt_11, #mt_12, #mt_13, #mt_14, #mt_15, #mt_16, #mt_17, #dot_1, #dot_3, #dot_4, #dot_5, #dot_2, #dot_7, #dot_8, #dot_9, #dot_6, #dot_11, #dot_12, #dot_13, #dot_14, #dot_15, #dot_16, #dot_17, #dot_18, #dot_19, #dot_20, #dot_21, #dot_22, #dot_23, #dot_24").css("opacity", "0.2");
+        }, function()
+        {
+          $("#mt_1, #mt_3,#mt_4, #mt_5, #mt_2, #mt_7, #mt_8, #mt_9, #mt_6, #mt_11, #mt_12, #mt_13, #mt_14, #mt_15, #mt_16, #mt_17,#dot_1, #dot_3, #dot_4, #dot_5, #dot_2, #dot_7, #dot_8, #dot_9, #dot_6, #dot_11, #dot_12, #dot_13, #dot_14, #dot_15, #dot_16, #dot_17").css("opacity", "1.0");
+        }
+      )
+
+      $("#dot_11").hover(function()
+          {
+            $("#mt_1, #mt_3,#mt_4, #mt_5, #mt_2, #mt_7, #mt_8, #mt_9, #mt_6, #mt_10, #mt_12, #mt_13, #mt_14, #mt_15, #mt_16, #mt_17, #dot_1, #dot_3, #dot_4, #dot_5, #dot_2, #dot_7, #dot_8, #dot_9, #dot_6, #dot_10, #dot_12, #dot_13, #dot_14, #dot_15, #dot_16, #dot_17, #dot_18, #dot_19, #dot_20, #dot_21, #dot_22, #dot_23, #dot_24").css("opacity", "0.2");
+          }, function()
+          {
+            $("#mt_1, #mt_3,#mt_4, #mt_5, #mt_2, #mt_7, #mt_8, #mt_9, #mt_6, #mt_10, #mt_12, #mt_13, #mt_14, #mt_15, #mt_16, #mt_17,#dot_1, #dot_3, #dot_4, #dot_5, #dot_2, #dot_7, #dot_8, #dot_9, #dot_6, #dot_10, #dot_12, #dot_13, #dot_14, #dot_15, #dot_16, #dot_17").css("opacity", "1.0");
+          }
+        )
+
+        $("#dot_12").hover(function()
+            {
+              $("#mt_1, #mt_3,#mt_4, #mt_5, #mt_2, #mt_7, #mt_8, #mt_9, #mt_6, #mt_11, #mt_10, #mt_13, #mt_14, #mt_15, #mt_16, #mt_17,  #dot_1, #dot_3, #dot_4, #dot_5, #dot_2, #dot_7, #dot_8, #dot_9, #dot_6, #dot_11, #dot_10, #dot_13, #dot_14, #dot_15, #dot_16, #dot_17, #dot_18, #dot_19, #dot_20, #dot_21, #dot_22, #dot_23, #dot_24").css("opacity", "0.2");
+            }, function()
+            {
+              $("#mt_1, #mt_3,#mt_4, #mt_5, #mt_2, #mt_7, #mt_8, #mt_9, #mt_6, #mt_11, #mt_10, #mt_13, #mt_14, #mt_15, #mt_16, #mt_17,  #dot_1, #dot_3, #dot_4, #dot_5, #dot_2, #dot_7, #dot_8, #dot_9, #dot_6, #dot_11, #dot_10, #dot_13, #dot_14, #dot_15, #dot_16, #dot_17").css("opacity", "1.0");
+            }
+          )
+
+          $("#dot_13").hover(function()
+              {
+                $("#mt_1, #mt_3,#mt_4, #mt_5, #mt_2, #mt_7, #mt_8, #mt_9, #mt_6, #mt_11, #mt_12, #mt_10, #mt_14, #mt_15, #mt_16, #mt_17, #dot_1, #dot_3, #dot_4, #dot_5, #dot_2, #dot_7, #dot_8, #dot_9, #dot_6, #dot_11, #dot_12, #dot_10, #dot_14, #dot_15, #dot_16, #dot_17, #dot_18, #dot_19, #dot_20, #dot_21, #dot_22, #dot_23, #dot_24").css("opacity", "0.2");
+              }, function()
+              {
+                $("#mt_1, #mt_3,#mt_4, #mt_5, #mt_2, #mt_7, #mt_8, #mt_9, #mt_6, #mt_11, #mt_12, #mt_10, #mt_14, #mt_15, #mt_16, #mt_17, #dot_1, #dot_3, #dot_4, #dot_5, #dot_2, #dot_7, #dot_8, #dot_9, #dot_6, #dot_11, #dot_12, #dot_10, #dot_14, #dot_15, #dot_16, #dot_17").css("opacity", "1.0");
+              }
+            )
+            $("#dot_14").hover(function()
+                {
+                  $("#mt_1, #mt_3,#mt_4, #mt_5, #mt_2, #mt_7, #mt_8, #mt_9, #mt_6, #mt_11, #mt_12, #mt_13, #mt_10, #mt_15, #mt_16, #mt_17,  #dot_1, #dot_3, #dot_4, #dot_5, #dot_2, #dot_7, #dot_8, #dot_9, #dot_6, #dot_11, #dot_12, #dot_13, #dot_10, #dot_15, #dot_16, #dot_17, #dot_18, #dot_19, #dot_20, #dot_21, #dot_22, #dot_23, #dot_24").css("opacity", "0.2");
+                }, function()
+                {
+                  $("#mt_1, #mt_3,#mt_4, #mt_5, #mt_2, #mt_7, #mt_8, #mt_9, #mt_6, #mt_11, #mt_12, #mt_13, #mt_10, #mt_15, #mt_16, #mt_17, #dot_1, #dot_3, #dot_4, #dot_5, #dot_2, #dot_7, #dot_8, #dot_9, #dot_6, #dot_11, #dot_12, #dot_13, #dot_10, #dot_15, #dot_16, #dot_17").css("opacity", "1.0");
+                }
+              )
+
+              $("#dot_15").hover(function()
+                  {
+                    $("#mt_1, #mt_3,#mt_4, #mt_5, #mt_2, #mt_7, #mt_8, #mt_9, #mt_6, #mt_11, #mt_12, #mt_13, #mt_14, #mt_10, #mt_16, #mt_17, #dot_1, #dot_3, #dot_4, #dot_5, #dot_2, #dot_7, #dot_8, #dot_9, #dot_6, #dot_11, #dot_12, #dot_13, #dot_14, #dot_10, #dot_16, #dot_17, #dot_18, #dot_19, #dot_20, #dot_21, #dot_22, #dot_23, #dot_24").css("opacity", "0.2");
+                  }, function()
+                  {
+                    $("#mt_1, #mt_3,#mt_4, #mt_5, #mt_2, #mt_7, #mt_8, #mt_9, #mt_6, #mt_11, #mt_12, #mt_13, #mt_14, #mt_10, #mt_16, #mt_17, #dot_1, #dot_3, #dot_4, #dot_5, #dot_2, #dot_7, #dot_8, #dot_9, #dot_6, #dot_11, #dot_12, #dot_13, #dot_14, #dot_10, #dot_16, #dot_17").css("opacity", "1.0");
+                  }
+                )
+
+                $("#dot_16").hover(function()
+                    {
+                      $("#mt_1, #mt_3,#mt_4, #mt_5, #mt_2, #mt_7, #mt_8, #mt_9, #mt_6, #mt_11, #mt_12, #mt_13, #mt_14, #mt_10, #mt_15, #mt_17,#dot_1, #dot_3, #dot_4, #dot_5, #dot_2, #dot_7, #dot_8, #dot_9, #dot_6, #dot_11, #dot_12, #dot_13, #dot_14, #dot_10, #dot_15, #dot_17, #dot_18, #dot_19, #dot_20, #dot_21, #dot_22, #dot_23, #dot_24").css("opacity", "0.2");
+                    }, function()
+                    {
+                      $("#mt_1, #mt_3,#mt_4, #mt_5, #mt_2, #mt_7, #mt_8, #mt_9, #mt_6, #mt_11, #mt_12, #mt_13, #mt_14, #mt_10, #mt_15, #mt_17,#dot_1, #dot_3, #dot_4, #dot_5, #dot_2, #dot_7, #dot_8, #dot_9, #dot_6, #dot_11, #dot_12, #dot_13, #dot_14, #dot_10, #dot_15, #dot_17").css("opacity", "1.0");
+                    }
+                  )
+
+
+                  $("#dot_17").hover(function()
+                      {
+                        $("#mt_1, #mt_3,#mt_4, #mt_5, #mt_2, #mt_7, #mt_8, #mt_9, #mt_6, #mt_11, #mt_12, #mt_13, #mt_14, #mt_10, #mt_15, #mt_16, #dot_1, #dot_3, #dot_4, #dot_5, #dot_2, #dot_7, #dot_8, #dot_9, #dot_6, #dot_11, #dot_12, #dot_13, #dot_14, #dot_10, #dot_15, #dot_16, #dot_18, #dot_19, #dot_20, #dot_21, #dot_22, #dot_23, #dot_24").css("opacity", "0.2");
+                      }, function()
+                      {
+                        $("#mt_1, #mt_3,#mt_4, #mt_5, #mt_2, #mt_7, #mt_8, #mt_9, #mt_6, #mt_11, #mt_12, #mt_13, #mt_14, #mt_10, #mt_15, #mt_16,  #dot_1, #dot_3, #dot_4, #dot_5, #dot_2, #dot_7, #dot_8, #dot_9, #dot_6, #dot_11, #dot_12, #dot_13, #dot_14, #dot_10, #dot_15, #dot_16").css("opacity", "1.0");
+                      }
+                    )
